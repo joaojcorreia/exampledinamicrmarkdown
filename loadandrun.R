318350c2a7b1f5078922f49e9c367cbfbd6fa5e1
João Correia <40802893+joaojcorreia@users.noreply.github.com>
2022-01-19 22:34:00 +0000

    Comit with working code

--- loadandrun.R
+++ loadandrun.R
@@ -17,18 +17,16 @@ fulldata <- read.xlsx ('MyData.xlsx', 1) #read the excel and store the data-fram
   
   while(a <= n ){
     
-    cat(a,' out of ',n)
+    cat(a,' out of ',n) #print out the report number
     data_individual <<- fulldata[a, ] #load one line, corresponding to one individual
     data_individual[is.na(data_individual)] <- '' #remove all NA
-    dados_grafico()
-    rmarkdown::render(input = "MyReport.Rmd", output_file = 'myreport.pdf', output_dir = "reports")
-    myfilename <- as.character(  #Generate a file name base on each individual first and last name
-      str_glue(
-        str_replace_all(data_individual[1], " ", "."),
-        str_replace_all(data_individual[2], " ", "."),
-        "pdf", .sep="."))
-    file.rename( '~/reports/myreport.pdf', str_glue('~/relatorios/',myfilename)) #change the file name to the individual name. I chose to to it like this because there are issues with the character encoding if the sistem is not in UTF8
-    a <- a+1
+    rmarkdown::render(input = "MyReport.Rmd", 
+                      output_file =  as.character(  #Generate a file name base on each individual first and last name
+                          str_glue(
+                            str_replace_all(data_individual[1], " ", "."),
+                            str_replace_all(data_individual[2], " ", "."),
+                            "pdf", .sep=".")), output_dir = "reports")
+      a <- a+1
     
   }
 }
